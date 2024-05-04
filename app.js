6b0e3d5e43e001167066b76ccd95b8d26c2e2a47
satya-2003 <138389036+satya-2003@users.noreply.github.com>
2024-05-04 15:42:50 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,56 @@
+
+
+function toggleCartPopup() {
+   
+       const cartPopup = document.getElementById('cart-popup');
+       cartPopup.classList.toggle('active');
+}
+    // for close cart popup
+function closeCart() {
+    const cartPopup = document.getElementById('cart-popup');
+    cartPopup.classList.remove('active')
+}
+
+function addToCart(itemName, itemPrice)  {
+
+    const cartItems = document.getElementById('cart-items').getElementsByTagName('tbody')[0];
+    const existingItem = Array.from(cartItems.getElementsByTagName('tr')).find(item => item.cells[0].textContent === itemName);
+
+    if (existingItem) {
+        const itemCount = parseInt(existingItem.querySelector('.item-count').textContent) + 1;
+        existingItem.querySelector('.item-count').textContent = itemCount;
+
+        const itemTotal = parseFloat(existingItem.querySelector('.item-total').textContent) + parseFloat(itemPrice);
+        existingItem.querySelector('.item-total').textContent = itemTotal.toFixed(2);
+    }
+
+    else {
+        const newRow = cartItems.insertRow();
+        newRow.innerHTML = `
+        <td>${itemName}</td>
+        <td class='item-count'>1</td>
+        <td class='item-price'>${itemPrice}</td>
+        <td class='item-total'>${itemPrice}</td>
+        `;
+    }
+    updateCartCountAndTotal();
+}
+
+
+function updateCartCountAndTotal() {
+    const cartCount = document.getElementById('cart-count');
+    const cartTotal = document.getElementById('card-total');
+    const cartItems = document.querySelectorAll('#cart-items tbody tr');
+    let totalCount=0;
+    let total = 0;
+    cartItems.forEach(item => {
+        const itemCount = parseInt(item.querySelector('.item-count').textContent);
+        const itemTotal = parseFloat(item.querySelector('.item-total').textContent);
+        totalCount += itemCount;
+        total += itemTotal;
+    });
+    cartCount.textContent = totalCount;
+    cartTotal.textContent = total.toFixed(2); 
+}
+
+
